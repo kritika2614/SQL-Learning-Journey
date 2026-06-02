052a6738a7659522209811383f6068d3aacf2a50
kritika2614 <krsharma2614@gmail.com>
2026-06-03 00:37:39 +0530

    Update create_tables.sql

--- Projects/HR_Analytics/create_tables.sql
+++ Projects/HR_Analytics/create_tables.sql
@@ -1,1 +1,23 @@
-Creating Projects
+CREATE TABLE Departments (
+    Dep_ID INT PRIMARY KEY,
+    Dep_Name VARCHAR(50)
+);
+
+CREATE TABLE Employees (
+    Emp_ID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Salary INT,
+    Dep_ID INT,
+    Manager_ID INT
+);
+
+CREATE TABLE Projects (
+    Project_ID INT PRIMARY KEY,
+    Project_Name VARCHAR(100),
+    Emp_ID INT
+);
+
+CREATE TABLE Attendance (
+    Emp_ID INT,
+    Days_Present INT
+);

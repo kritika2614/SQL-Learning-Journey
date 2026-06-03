a965189de61562e7b407ca2ce794cf2df7aed5a0
kritika2614 <krsharma2614@gmail.com>
2026-06-03 16:35:40 +0530

    Update create_tables.sql

--- Projects/HR_Analytics/create_tables.sql
+++ Projects/HR_Analytics/create_tables.sql
@@ -21,3 +21,24 @@ CREATE TABLE Attendance (
     Emp_ID INT,
     Days_Present INT
 );
+#Which Department is the most expensive for the company:
+SELECT d.DEP_ID, Dep_Name, SUM(SALARY), AVG(SALARY) AS AVGSAL, COUNT(e.Emp_ID) as EmpCount
+FROM Departments as d
+LEFT JOIN Employees as e
+ON d.Dep_ID = e.Dep_ID
+GROUP BY d.Dep_ID, d.Dep_Name
+ORDER BY SUM(Salary) DESC;
+
+#Which employees are paid above their department's average?
+SELECT Emp_ID, Salary, DEPSAL
+FROM (SELECT Emp_ID, Salary, AVG(Salary) OVER (Partition by Dep_ID) as DEPSAL FROM EMPLOYEES) AS Y
+WHERE SALARY > DEPSAL;  
+
+# Which employees may need attention due to low attendance?
+    SELECT *
+    FROM (SELECT e.Name, a.Days_Present, AVG(a.Days_Present) OVER () AS AVGATT FROM Employees AS e
+    JOIN Attendance as a
+    ON e.Emp_ID = a.Emp_ID) as Y
+    WHERE Days_Present<AvgATT;
+    
+
